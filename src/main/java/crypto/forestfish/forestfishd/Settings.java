a8904e7550062147e28e07c25600a3a929603778
p00temkin <p00temkin@gmail.com>
2023-06-29 23:32:56 +0200

    forestfishd API pojos moved to forestfish

--- src/main/java/crypto/forestfish/forestfishd/Settings.java
+++ src/main/java/crypto/forestfish/forestfishd/Settings.java
@@ -1,8 +1,13 @@
 package crypto.forestfish.forestfishd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import crypto.forestfish.utils.SystemUtils;
 
 public class Settings {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Settings.class);
 
 	private int port = 6969;
 	private String jwtSecret;
@@ -20,7 +25,7 @@ public class Settings {
 	public void setPort(int port) {
 		this.port = port;
 	}
-	
+
 	public String getJwtSecret() {
 		return jwtSecret;
 	}
@@ -38,14 +43,14 @@ public class Settings {
 	}
 
 	public void print() {
-        System.out.println("Settings:");
-        System.out.println(" - port: " + this.getPort());
-        if (null != this.getJwtSecret()) System.out.println(" - jwtsecret length: " + this.getJwtSecret().length());
-        System.out.println(" - nftmode: " + this.isNftmode());
-        System.out.println(" - tokenmode: " + this.isTokenmode());
-    }
-	
-    public boolean isTokenmode() {
+		System.out.println("Settings:");
+		System.out.println(" - port: " + this.getPort());
+		if (null != this.getJwtSecret()) System.out.println(" - jwtsecret length: " + this.getJwtSecret().length());
+		System.out.println(" - nftmode: " + this.isNftmode());
+		System.out.println(" - tokenmode: " + this.isTokenmode());
+	}
+
+	public boolean isTokenmode() {
 		return tokenmode;
 	}
 
@@ -54,14 +59,25 @@ public class Settings {
 	}
 
 	public void sanityCheck() {
-    	
-    	// sane port
-    	if ((this.port > 0) && (this.port <= 65535))  {
-    		// ok
-    	} else {
-            System.out.println(" - Invalid port: " + this.getPort());
-            SystemUtils.halt();
-        }
 
-    }
+		// sane port
+		if ((this.port > 0) && (this.port <= 65535))  {
+			// ok
+		} else {
+			LOGGER.error(" - Invalid port: " + this.getPort());
+			SystemUtils.halt();
+		}
+
+		// sane JWT secret
+		if (null == this.jwtSecret) {
+			LOGGER.error(" - The JWT secret needs to be defined");
+			SystemUtils.halt();
+		}
+		if (!(this.jwtSecret.length() >= 50))  {
+			LOGGER.error(" - The JWT secret needs to be at least 50 characters, current length is only " + this.jwtSecret.length());
+			SystemUtils.halt();
+		}
+
+	}
+
 }
--- src/main/java/crypto/forestfish/forestfishd/model/policy/Role.java
+++
@@ -1,8 +0,0 @@
-package crypto.forestfish.forestfishd.model.policy;
-
-public enum Role {
-	CONSUMER,
-	CONTRIBUTOR,
-	ADMIN,
-	UNDEFINED
-}
